68ebe50acd8b14365d326f2f68a1efc5151de836
revsic <revsic99@gmail.com>
2021-12-04 18:45:39 +0900

    encoder: Rename to nat

--- model/nat.py
+++ model/nat.py
@@ -4,10 +4,11 @@ import torch.nn as nn
 from .cbhg import Cbhg
 from .config import Config
 from .misc import Prenet
+from .upsampler import Upsampler
 
 
-class Encoder(nn.Module):
-    """Text encoder.
+class NonAttentiveTacotron(nn.Module):
+    """Non-attentive tacotron for multispeakers
     """
     def __init__(self, config: Config):
         """Initializer.
@@ -18,20 +19,24 @@ class Encoder(nn.Module):
         self.embedding = nn.Embedding(config.vocabs, config.embeddings)
         self.prenet = Prenet(
             config.embeddings,
-            config.enc_prenet + [config.channels],
+            config.enc_prenet + [config.channels // 2],
             config.enc_dropout)
 
         self.cbhg = Cbhg(
-            config.channels,
+            config.channels // 2,
             config.cbhg_banks,
             config.cbhg_pool,
             config.cbhg_kernels,
             config.cbhg_highways)
 
-    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
+        self.upsampler = Upsampler(
+            config.channels, config.upsampler_layers)
+
+    def forward(self, inputs: torch.Tensor, textlen: torch.Tensor) -> torch.Tensor:
         """Encode text tokens.
         Args;
             inputs: [torch.long; [B, S]], text symbol sequences.
+            textlen: [torch.long; [B]], sequence lengths.
         Returns:
             [torch.float32; [B, S, C x 2]], CBHG features.
         """
